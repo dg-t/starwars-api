2bf83aec594e4a3527cf7d6ca9badac8ff8d497d
orione_90@hotmail.com <orione_90@hotmail.com>
2021-05-20 14:29:39 +0200

    Add basic error handling for API call, display people details

--- src/components/People/PeopleDetails.vue
+++ src/components/People/PeopleDetails.vue
@@ -1,22 +1,30 @@
 <template>
     <section class="container-fluid">
-        <h1>People detail</h1>
-        <p>name: {{ name }}</p>
-        <p>height: {{ height }}</p>
-        <p>mass: {{ mass }}</p>
-        <p>hairColor: {{ hairColor }}</p>
-        <p>eyeColor: {{ eyeColor }}</p>
-        <p>birthYear: {{ birthYear }}</p>
-        <p>gender: {{ gender }}</p>
-        <p>planet: {{ planet }}</p>
         <div>
-            <span>Films: <span v-for="(film, index) in films" :key="index">{{ film }}. </span></span>
+            <h1>People detail</h1>
+            <p>Name: {{ name }}</p>
+            <p>Height: {{ height }}</p>
+            <p>Mass: {{ mass }}</p>
+            <p>Hair color: {{ hairColor }}</p>
+            <p>Eye color: {{ eyeColor }}</p>
+            <p>Birth year: {{ birthYear }}</p>
+            <p>Gender: {{ gender }}</p>
+            <p>Planet: {{ planet }}</p>
         </div>
         <div>
-            <span>Vehicles: <span v-for="(vehicle, index) in vehicles" :key="index">{{ vehicle }}. </span></span>
+            <p v-if="films.length > 0">Films: <span v-for="(film, index) in films" :key="index">{{ film }}. </span></p>
         </div>
         <div>
-            <span>Starships: <span v-for="(starship, index) in starships" :key="index">{{ starship }}. </span></span>
+            <p v-if="species.length > 0">Species: <span v-for="(specie, index) in species" :key="index">{{ specie }}. </span></p>
+        </div>
+        <div>
+            <p v-if="vehicles.length > 0">Vehicles: <span v-for="(vehicle, index) in vehicles" :key="index">{{ vehicle }}. </span></p>
+        </div>
+        <div>
+            <p v-if="starships.length > 0">Starships: <span v-for="(starship, index) in starships" :key="index">{{ starship }}. </span></p>
+        </div>
+        <div>
+            <router-link to="/people/" class="btn btn-primary">Go back</router-link>
         </div>
     </section>
 </template>
@@ -35,65 +43,82 @@ export default {
             gender: '',
             planet: '',
             films: [],
+            species: [],
             vehicles: [],
             starships: []
-
         }
     },
     methods: {
         async getDetails() {
-            const peopleId = (this.$route.params.peopleId)*1+1;
-            
-            // GENERAL INFO
-            const req = await fetch('https://swapi.dev/api/people/'+peopleId); 
-            const res = await req.json();
+            try {
 
-            this.name = res.name;
-            this.height = res.height;
-            this.mass = res.mass;
-            this.hairColor = res.hair_color;
-            this.eyeColor = res.eye_color;
-            this.birthYear = res.birth_year;
-            this.gender = res.gender;
+                // GET peopleId FROM ROUTE PARAMS
+                const peopleId = this.$route.params.peopleId;
 
-            // HOMEWORLD
+                if (peopleId) {
+                
+                    // GENERAL INFO
+                    const req = await fetch('https://swapi.dev/api/people/'+peopleId); 
+                    const res = await req.json();
+                    
+                    if (res) {
+                        this.name = res.name;
+                        this.height = res.height;
+                        this.mass = res.mass;
+                        this.hairColor = res.hair_color;
+                        this.eyeColor = res.eye_color;
+                        this.birthYear = res.birth_year;
+                        this.gender = res.gender;
+                    }
 
-            const reqHomeworld = await fetch(res.homeworld); 
-            const resHomeworld = await reqHomeworld.json();
+                    // HOMEWORLD
+                    if (res.homeworld) {
+                        const reqHomeworld = await fetch(res.homeworld); 
+                        const resHomeworld = await reqHomeworld.json();
 
-            this.planet = resHomeworld.name;
+                        this.planet = resHomeworld.name;
+                    }
 
-            // FILMS
-            for (let i = 0; i < res.films.length; i++) {
-                let reqFilms = await fetch(res.films[i]);
-                let resFilms= await reqFilms.json();
-                this.films.push(resFilms.title);
+                    // FILMS
+                    if (res.films.length > 0) {
+                        for (let i = 0; i < res.films.length; i++) {
+                            let reqFilms = await fetch(res.films[i]);
+                            let resFilms= await reqFilms.json();
+                            this.films.push(resFilms.title);
+                        }
+                    }
+
+                    // SPECIES
+                    if (res.species.length > 0) {
+                        for (let i = 0; i < res.species.length; i++) {
+                            let reqSpecies = await fetch(res.species[i]);
+                            let resSpecies= await reqSpecies.json();
+                            this.species.push(resSpecies.name);
+                        }
+                    }
+
+                    // VEHICLES
+                    if (res.vehicles.length > 0) {
+                        for (let i = 0; i < res.vehicles.length; i++) {
+                            let reqVehicles = await fetch(res.vehicles[i]);
+                            let resVehicles= await reqVehicles.json();
+                            this.vehicles.push(resVehicles.name);
+                        }
+                    }
+
+                    // STARSHIPS
+                    if (res.starships.length > 0) {
+                        for (let i = 0; i < res.starships.length; i++) {
+                            let reqStarships = await fetch(res.starships[i]);
+                            let resStarships= await reqStarships.json();
+                            this.starships.push(resStarships.name);
+                        }
+                    }
+                }
+            } catch (error) {
+                alert('Something went wrong...');
+                console.log(error);
             }
-
-            // SPECIES
-            for (let i = 0; i < res.species.length; i++) {
-                let reqSpecies = await fetch(res.species[i]);
-                let resSpecies= await reqSpecies.json();
-                this.films.push(resSpecies.name);
-            }
-
-            // VEHICLES
-            for (let i = 0; i < res.vehicles.length; i++) {
-                let reqVehicles = await fetch(res.vehicles[i]);
-                let resVehicles= await reqVehicles.json();
-                this.vehicles.push(resVehicles.name);
-            }
-
-            // STARSHIPS
-            for (let i = 0; i < res.starships.length; i++) {
-                let reqStarships = await fetch(res.starships[i]);
-                let resStarships= await reqStarships.json();
-                this.starships.push(resStarships.name);
-                console.log(resStarships.name)
-            }
-
-        
-
         }
     },
     created() {
--- src/components/People/PeopleList.vue
+++ src/components/People/PeopleList.vue
@@ -3,7 +3,10 @@
         <h1>People</h1>
         <div v-for="(p, index) in people"
             :key="index">
-            <router-link :to="'/people/'+index" class="btn btn-primary">{{p.name}}</router-link>
+            <router-link :to="'/people/'+p.url.split('/').slice(1).slice(-2).join('/')" class="btn btn-primary">{{p.name}}</router-link>
+        </div>
+        <div>
+            <router-link to="/" class="btn btn-primary">Go back</router-link>
         </div>
     </section>
 </template>
@@ -19,14 +22,20 @@ export default {
     },
     methods: {
         async getPeople() {
+            // TRY GET ALL PEOPLE FROM SWAPI
+            try {
+                const req = await fetch('https://swapi.dev/api/people'); 
+                const res = await req.json();
 
-            const req = await fetch('https://swapi.dev/api/people'); 
-            const res = await req.json();
-
-            for (let p = 0; p < res.results.length; p++) {
-                this.people.push(res.results[p])
+                if (res) {
+                    for (let p = 0; p < res.results.length; p++) {
+                        this.people.push(res.results[p])
+                    }
+                }
+            } catch (error) {
+                alert('Something went wrong...');
+                console.log(error)
             }
-            console.log(this.people[0])
         },
         /** THEN */
         // getPeople() {
